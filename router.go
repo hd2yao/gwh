83231a9295e6a869b525fffe00e7723f3e078a3b
hd2yao <1054290355@qq.com>
2024-10-22 10:46:31 +0800

    feat(router): 修改 handler 逻辑，将路由参数保存

--- router.go
+++ router.go
@@ -64,11 +64,12 @@ func (r *router) getRoute(method, path string) (*node, map[string]string) {
 }
 
 func (r *router) handle(c *Context) {
-    key := c.Method + "-" + c.Path
-    handler, ok := r.handlers[key]
-    if !ok {
+    searchNode, params := r.getRoute(c.Method, c.Path)
+    if searchNode != nil {
+        c.Params = params
+        key := c.Method + "-" + c.Path
+        r.handlers[key](c)
+    } else {
         c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
-        return
     }
-    handler(c)
 }
