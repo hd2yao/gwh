fd9a9a2983898f7ec53ad6b2efa69841f2a87518
hd2yao <1054290355@qq.com>
2024-10-22 10:36:14 +0800

    feat(router): 构建结构体 router 并添加方法

--- router.go
+++ router.go
@@ -1,25 +1,66 @@
 package gwh
 
 import (
-    "log"
     "net/http"
+    "strings"
 )
 
 type router struct {
+    roots    map[string]*node
     handlers map[string]HandlerFunc
 }
 
+// roots key eg, roots['GET'] roots['POST']
+// handlers key eg, handlers['GET-/p/:lang/doc'], handlers['POST-/p/book']
+
 func newRouter() *router {
     return &router{
+        roots:    make(map[string]*node),
         handlers: make(map[string]HandlerFunc),
     }
 }
 
 // addRoute 添加路由
 func (r *router) addRoute(method, pattern string, handler HandlerFunc) {
-    log.Printf("Route %4s - %s", method, pattern)
+    parts := parsePattern(pattern)
     key := method + "-" + pattern
+    if _, exists := r.roots[method]; !exists {
+        r.roots[method] = &node{}
+    }
+    r.roots[method].insert(pattern, parts, 0)
     r.handlers[key] = handler
+}
+
+// getRoute 通过请求路径获取路由节点
+func (r *router) getRoute(method, path string) (*node, map[string]string) {
+    root, exists := r.roots[method]
+    if !exists {
+        return nil, nil
+    }
+
+    // 请求路径解析
+    searchParts := parsePattern(path)
+    // 查找对应路由节点
+    searchNode := root.search(searchParts, 0)
+    if searchNode != nil {
+        // 用户模糊匹配中，保存请求的参数
+        params := make(map[string]string)
+        parts := parsePattern(searchNode.pattern)
+        for i, part := range parts {
+            if part[0] == ':' {
+                // 路由为 :id，则保存为 ["id", "123"]
+                params[part[1:]] = searchParts[i]
+            }
+            if part[0] == '*' && len(part) > 1 {
+                // 路由为 *filepath，则保存为 ["filepath", "images/pic.jpg"]
+                params[part[1:]] = strings.Join(searchParts[i:], "/")
+                break
+            }
+        }
+        return searchNode, params
+    }
+
+    return nil, nil
 }
 
 func (r *router) handle(c *Context) {
