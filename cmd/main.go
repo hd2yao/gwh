228ccc14f4903661a6df6f64f93a229679fb2331
hd2yao <1054290355@qq.com>
2024-10-23 16:24:41 +0800

    feat(main): 修改 main 中调用，构造 group 分组路由

--- cmd/main.go
+++ cmd/main.go
@@ -8,27 +8,39 @@ import (
 
 func main() {
     engine := gwh.New()
-    engine.GET("/", func(c *gwh.Context) {
-        c.HTML(http.StatusOK, "<h1>Hello Gwh</h1>")
-    })
-    engine.GET("/hello", func(c *gwh.Context) {
-        c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+    engine.GET("/index", func(c *gwh.Context) {
+        c.HTML(http.StatusOK, "<h1>Index Page</h1>")
     })
 
-    engine.GET("/hello/:name", func(c *gwh.Context) {
-        // expect /hello/geektutu
-        c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-    })
+    v1 := engine.Group("/v1")
+    {
+        v1.GET("/", func(c *gwh.Context) {
+            c.HTML(http.StatusOK, "<h1>Hello Gwh</h1>")
+        })
+
+        v1.GET("/hello", func(c *gwh.Context) {
+            c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+        })
+    }
+
+    v2 := engine.Group("/v2")
+    {
+        v2.GET("/hello/:name", func(c *gwh.Context) {
+            // expect /hello/hai
+            c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+        })
+
+        v2.POST("/login", func(c *gwh.Context) {
+            c.JSON(http.StatusOK, gwh.H{
+                "username": c.PostForm("username"),
+                "password": c.PostForm("password"),
+            })
+        })
+    }
 
     engine.GET("/assets/*filepath", func(c *gwh.Context) {
         c.JSON(http.StatusOK, gwh.H{"filepath": c.Param("filepath")})
     })
 
-    engine.POST("/login", func(c *gwh.Context) {
-        c.JSON(http.StatusOK, gwh.H{
-            "username": c.PostForm("username"),
-            "password": c.PostForm("password"),
-        })
-    })
     engine.Run(":9999")
 }
